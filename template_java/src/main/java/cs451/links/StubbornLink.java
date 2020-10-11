872b36e15d32b2742fb391acd4378388a018c732
MinhTri-Pham <s1612839@sms.ed.ac.uk>
2020-10-11 15:58:17 +0200

    Test

--- template_java/src/main/java/cs451/links/StubbornLink.java
+++ template_java/src/main/java/cs451/links/StubbornLink.java
@@ -9,32 +9,37 @@ import java.util.Set;
 
 
 public class StubbornLink {
-    private FairLossLink fllSend; // Channel for sending data
-    private FairLossLink fllRec; // Channel for receiving data/ACKs
+//    private FairLossLink fllSend; // Channel for sending data
+//    private FairLossLink fllRec; // Channel for receiving data/ACKs
+    private FairLossLink fll; // Channel for receiving data/ACKs
     private Set<Integer> notAcked; // Messages not acknowledged yet
 
     public StubbornLink(int sourcePort, InetAddress sourceIp) {
-        this.fllSend = new FairLossLink();
-        this.fllRec = new FairLossLink(sourcePort, sourceIp);
+//        this.fllSend = new FairLossLink();
+//        this.fllRec = new FairLossLink(sourcePort, sourceIp);
+        this.fll = new FairLossLink(sourcePort, sourceIp);
         this.notAcked = new HashSet<>();
     }
 
     public void send(Message message, int destPort, InetAddress destIp) throws IOException {
         System.out.println("Sending message " + message);
-        fllSend.send(message, destPort, destIp);
+//        fllSend.send(message, destPort, destIp);
+        fll.send(message, destPort, destIp);
         notAcked.add(message.getSeqNum());
     }
 
 
     public Message receive() throws IOException {
-        Message received = fllRec.receive();
+//        Message received = fllRec.receive();
+        Message received = fll.receive();
         System.out.println("Received message " + received);
         int seqNum = received.getSeqNum();
         // Received data, send ACK
         if (!received.isAck() && notAcked.contains(seqNum)) {
             System.out.println("Sending ACK to " + received);
             Message ackMessage = received.generateAck();
-            fllSend.send(ackMessage, received.getSourcePort(), received.getSourceIp());
+//            fllSend.send(ackMessage, received.getSourcePort(), received.getSourceIp());
+            fll.send(ackMessage, received.getSourcePort(), received.getSourceIp());
             notAcked.remove(seqNum);
         }
         return received;
